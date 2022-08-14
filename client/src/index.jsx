e19d9c8b8e13b0447b74f49af07fa2230d67a450
Morgan Harrison <morganharrison13@gmail.com>
2022-08-14 15:18:53 -0600

    Created jsx files for reviewsList, deleted example.env and configured .env locally, wrote componentDidMount to get initial list of reviews

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,29 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const ratingsAndReviews = require('./ratingsAndReviews.jsx');
+const axios = require('axios');
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    this.state = {reviews: []}
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/reviews')
+      .then((response) => {
+        console.log('response.data: ', response.data);
+        this.setState(reviews: response.data.results);
+      })
+      .catch((err) => {
+        throw err;
+      })
   }
 
   render() {
     return (
       <div>
-        Hello From React
+        <ratingsAndReviews />
       </div>
     )
   }
