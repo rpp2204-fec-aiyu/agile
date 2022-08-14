6bef1b7971be41ba3f8b59d1a11b592c7918dadb
Morgan Harrison <morganharrison13@gmail.com>
2022-08-14 16:31:57 -0600

    refactored index.js to use createRoot

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,32 +1,20 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
-const ratingsAndReviews = require('./ratingsAndReviews.jsx');
-const axios = require('axios');
+const {createRoot} = require('react-dom/client')
+import RatingsAndReviews from './components/ratingsAndReviews.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {reviews: []}
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3000/reviews')
-      .then((response) => {
-        console.log('response.data: ', response.data);
-        this.setState(reviews: response.data.results);
-      })
-      .catch((err) => {
-        throw err;
-      })
   }
 
   render() {
     return (
       <div>
-        <ratingsAndReviews />
+        <RatingsAndReviews />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'))
+const root = createRoot( document.getElementById('app'));
+root.render(<App/>);
