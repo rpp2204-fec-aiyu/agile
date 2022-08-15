4ecfb49cbd36ebcc3382fce9dd6bf4d826c4299b
Nick Hays <nicholasahays@gmail.com>
2022-08-15 16:14:42 -0700

    Maps style thumbnails, currently in Gallery component

--- client/src/modules/ProductOverview/Gallery/Gallery.jsx
+++ client/src/modules/ProductOverview/Gallery/Gallery.jsx
@@ -5,8 +5,14 @@ export default function Gallery(props) {
 
 
   return (
-    <img src={props.style.photos[0].url} style={{maxWidth: '50%'}}
-         key={props.style.style_id}>
-    </img>
+    <div>
+      <img src={props.style.photos[0].url} style={{maxWidth: '50%'}}
+          key={props.style.style_id}>
+      </img>
+      <br></br>
+      {props.style.photos.map(photo => (
+        <img src={photo.thumbnail_url} width='75' height='75'></img>
+      ))}
+    </div>
   )
 }
