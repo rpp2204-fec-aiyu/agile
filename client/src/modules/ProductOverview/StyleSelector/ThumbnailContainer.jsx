fee0c2cf85930ad2f4257245882403f6bd6a8483
Nick Hays <nicholasahays@gmail.com>
2022-08-15 21:31:34 -0700

    Adds setSalePrice to handleClick function

--- client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
+++ client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
@@ -4,32 +4,20 @@ export default function ThumbnailContainer(props) {
 
   const [title, setTitle] = useState(props.styles[0].name)
 
-  //let title = props.styles[0].name;
-
-  // function handleClick(style) {
-  //   console.log('CLICKED!!')
-  //   title = style.name
-  // }
-
   function handleClick(style) {
     setTitle(style.name)
     props.setStyle(style)
-    if(style.sale_price) {
-      props.setPrice(style.sale_price)
-    } else {
-      props.setPrice(style.original_price)
-    }
 
+    props.setPrice(style.original_price)
+    props.setSalePrice(style.sale_price)
   }
-
-
 
   return (
     <div>
       <h4><strong>Style > </strong>{title}</h4>
       {props.styles.map(style => (
         <img src={style.photos[0].thumbnail_url}
-             onClick={()=>handleClick(style)} //was ()=>setTitle(style.name)
+             onClick={()=>handleClick(style)}
              width='75'
              height='75'
              style={{borderRadius: '50%'}}
