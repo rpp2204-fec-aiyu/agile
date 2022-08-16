e7fe2f04758df7c0f23963d70c2a5e2a6d3bceb7
Nick Hays <nicholasahays@gmail.com>
2022-08-15 21:35:12 -0700

    Adds dollar sign to price

--- client/src/modules/ProductOverview/ProductInfo/Price.jsx
+++ client/src/modules/ProductOverview/ProductInfo/Price.jsx
@@ -7,13 +7,13 @@ export default function Price(props) {
   if(props.salePrice) {
     return (
       <>
-      <div style={{color:'#FF0000'}}>Price: {props.salePrice}</div><div><s>{props.price}</s></div>
+      <div style={{color:'#FF0000'}}>Price: ${props.salePrice}</div><div><s>${props.price}</s></div>
       </>
     )
   } else {
     return(
       <div>
-        Price: {props.price}
+        Price: ${props.price}
       </div>
     )
   }
