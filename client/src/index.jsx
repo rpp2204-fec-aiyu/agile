c64f2a15fa5d80f3abc68fde9975d1e7efaf63ea
Nick Hays <nicholasahays@gmail.com>
2022-08-21 12:15:48 -0700

    Adds getCurrentProduct function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,15 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+import axios from 'axios'
+
+function App() {
+  const getCurrentProduct = () => {
+    return axios.get('/products')
+      .then(results => {
+        return results.data[4]
+      })
+  }
+}
 
 class App extends React.Component {
   constructor() {
