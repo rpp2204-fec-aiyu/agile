eaa3b843cebbc15f22e06cd1c4c2c47dad1f138c
Nick Hays <nicholasahays@gmail.com>
2022-08-21 13:39:50 -0700

    Adds middleware and sets up product api call

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
+const PORT = process.env.PORT
+const APIKEY = process.env.APIKEY
+const BASEURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
+
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT
 
 app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/products', (req, res) => {
+  axios.get(`${BASEURL}/products`, {headers: {Authorization: APIKEY}})
+    .then(products => {
+      res.status(200).send(products)
+    })
+    .catch(err => {
+      console.log(err)
+      res.send(err)
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
