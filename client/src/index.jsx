6e9f2da4ab9a5803365df6f4c958c4bb1a8bb0c5
lingxuan xu <matt7xu@gmail.com>
2022-08-23 21:52:22 -0700

    skeleton of related products

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,23 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
 
 class App extends React.Component {
-  constructor() {
-    super()
-  }
 
   render() {
     return (
-      <div>
-        Hello From React
-      </div>
+      <>
+
+        <RelatedProducts />
+      </>
+
     )
   }
+
 }
+
 
 ReactDOM.render(<App/>, document.getElementById('app'))
--- server/index.js
+++ server/index.js
@@ -1,9 +1,41 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const APIKEY = process.env.APIKEY
+const BASEURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
+const axios = require('axios');
 
-app.use(express.static(__dirname + '/../client/dist'))
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+
+app.get('/products/:product_id/styles', (req, res) => {
+  let id = req.params.product_id
+  console.log('ID FROM URL',id)
+  axios.get(`${BASEURL}/products/${id}/styles`, {headers: {'Authorization': APIKEY}})
+    .then(results => {
+      res.status(200).send(results.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+
+app.get('/reviews', (req, res) => {
+  console.log('req.query: ', req.query);
+  axios.get(`${BASEURL}/reviews`, {
+    headers: {'Authorization': APIKEY},
+    params: req.query
+  })
+    .then((response) => {
+      // console.log(response.data)
+      res.status(200).send(response.data);
+    })
+    .catch((err) => { throw err; });
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
