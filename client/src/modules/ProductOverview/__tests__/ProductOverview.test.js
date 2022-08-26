cecd3ef2ff3ca65ddcf79471f1542406e391e3d1
Nick Hays <nicholasahays@gmail.com>
2022-08-25 20:45:40 -0700

    Creates passing component test, however, the snapshot was deleted and was not recreated

--- client/src/modules/ProductOverview/__tests__/ProductOverview.test.js
+++ client/src/modules/ProductOverview/__tests__/ProductOverview.test.js
@@ -1,9 +1,19 @@
+require("dotenv").config()
+const APIKEY = process.env.APIKEY
 import React from 'react'
 import renderer from 'react-test-renderer'
+import axios from 'axios'
 import ProductOverview from '../ProductOverview.jsx'
 
 it('Renders ProductOverview component', () => {
-  const component = renderer.create(<ProductOverview />)
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  axios.get('/products', {headers: {'Authorization': APIKEY}})
+    .then(products => {
+      return products.data[4]
+    })
+    .then(product => {
+      const component = renderer.create(<ProductOverview product={product} productId={product.id}/>)
+      let tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+
+    })
 })
