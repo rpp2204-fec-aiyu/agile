b5b6ec6db73ab6cd5dacab2337620d02ad1f4349
Morgan Harrison <morganharrison13@gmail.com>
2022-08-26 11:30:40 -0600

    Commiting recent productBreakdown component work

--- client/src/modules/RatingsReviews/productBreakdown.jsx
+++ client/src/modules/RatingsReviews/productBreakdown.jsx
@@ -13,35 +13,42 @@ export default class ProductBreakdown extends React.Component {
     var width = this.props.productWidthMetaData;
     var fit = this.props.productFitMetaData;
     var length = this.props.productLengthMetaData;
+    console.log('quality: ', quality);
 
     if (quality) {
       var qualityBreakdown =
       <div className='qualityBreakdown'>
+        <div className='feedback-average'>{quality.value}</div>
       </div>
     }
     if (size) {
       var sizeBreakdown =
       <div className='sizeBreakdown'>
+        <div className='feedback-average'>{size.value}</div>
       </div>
     }
     if (width) {
       var widthBreakdown =
       <div className='widthBreakdown'>
+        <div className='feedback-average'>{width.value}</div>
       </div>
     }
     if (comfort) {
       var comfortBreakdown =
       <div className='comfortBreakdown'>
+        <div className='feedback-average'>{comfort.value}</div>
       </div>
     }
     if (fit) {
       var fitBreakdown =
       <div className='fitBreakdown'>
+        <div className='feedback-average'>{fit.value}</div>
       </div>
     }
     if (length) {
       var lengthBreakdown =
       <div className='lengthBreakdown'>
+        <div className='feedback-average'>{length.value}</div>
       </div>
     }
     return (
