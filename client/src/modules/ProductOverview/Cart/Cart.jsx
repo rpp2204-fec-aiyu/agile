1c2f71d55054f253be91c91de6f351921472768f
Nick Hays <nicholasahays@gmail.com>
2022-08-26 23:19:11 -0700

    Adds useState for skuId and passes skuId as props to CartButton

--- client/src/modules/ProductOverview/Cart/Cart.jsx
+++ client/src/modules/ProductOverview/Cart/Cart.jsx
@@ -8,21 +8,23 @@ export default function Cart(props) {
   const [size, setSize] = useState(null)//useState(Object.entries(props.style.skus)[0][1].size)
   const [quantity, setQuantity] = useState(null)//useState(Object.entries(props.style.skus)[0][1].quantity)
   const [quantitySelection, setQuantitySelection] = useState(1)
+  const [skuId, setSkuId] = useState(null)
 
   function handleSizeQuantity(style) {
     setSize(style.size)
     setQuantity(style.quantity)
+    setSkuId(style.skuId)
   }
 
   useEffect(()=> {
-    console.log(size, quantity, quantitySelection)
-  }, [size, quantity, quantitySelection])
+    console.log(size, quantity, quantitySelection, skuId)
+  }, [size, quantity, quantitySelection, skuId])
 
   return (
     <>
       <Size style={props.style} setStyle={props.setStyle} handleSizeQuantity={handleSizeQuantity}/>
       <Quantity quantity={quantity} size={size} setQuantitySelection={setQuantitySelection}/>
-      <CartButton style={props.style} size={size} quantitySelection={quantitySelection} />
+      <CartButton style={props.style} size={size} quantitySelection={quantitySelection} skuId={skuId} />
     </>
   )
 
