25cc748057362abf502577aad5ed1162cb1cb86e
Nick Hays <nicholasahays@gmail.com>
2022-08-26 23:45:00 -0700

    Adds disabled dropdown if style is out of stock

--- client/src/modules/ProductOverview/Cart/Size.jsx
+++ client/src/modules/ProductOverview/Cart/Size.jsx
@@ -3,26 +3,35 @@ import React, { useState } from 'react'
 export default function Size(props) {
 
   //TODO if no quantities, dropdown should be disabled and read 'out of stock'
+  if(!Object.entries(props.style.skus).filter(sku => sku[1].quantity > 0).length) {
+    return (
+      <label>Size:
+        <select disabled>
+          <option>Out Of Stock</option>
+        </select>
+      </label>
+    )
+  } else {
+    return (
+      <label>Size:
+        <select id='sizeSelector' onChange={(e)=>props.handleSizeQuantity(
+            {
+              size: e.target.value,
+              quantity: e.target.selectedOptions[0].dataset.quantity,
+              skuId: e.target.selectedOptions[0].dataset.skuid
+            }
+          )}>
+
+          <option value='selectsize'>Select Size</option>
+          {Object.entries(props.style.skus).filter(sku => sku[1].quantity > 0).map((sku, i) => (  //check quantity > 0
+            <option data-quantity={sku[1].quantity} data-skuid={sku[0]} key={i}>{sku[1].size}</option>
+          ))}
+
+        </select>
+
+      </label>
+    )
+  }
 
 
-  return (
-    <label>Size:
-      <select id='sizeSelector' onChange={(e)=>props.handleSizeQuantity(
-          {
-            size: e.target.value,
-            quantity: e.target.selectedOptions[0].dataset.quantity,
-            skuId: e.target.selectedOptions[0].dataset.skuid
-          }
-        )}>
-
-        <option value='selectsize'>Select Size</option>
-        {Object.entries(props.style.skus).filter(sku => sku[1].quantity > 0).map((sku, i) => (  //check quantity > 0
-          <option data-quantity={sku[1].quantity} data-skuid={sku[0]} key={i}>{sku[1].size}</option>
-        ))}
-
-      </select>
-
-    </label>
-
-  )
 }
