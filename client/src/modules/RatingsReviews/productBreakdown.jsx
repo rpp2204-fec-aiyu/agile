be96c66bac455ded0338c12b42ebc1162c157a43
Morgan Harrison <morganharrison13@gmail.com>
2022-08-27 13:38:56 -0600

    Completed productBreakdown

--- client/src/modules/RatingsReviews/productBreakdown.jsx
+++ client/src/modules/RatingsReviews/productBreakdown.jsx
@@ -6,6 +6,10 @@ export default class ProductBreakdown extends React.Component {
     this.state = {}
   }
 
+  calculatePointerPosition(value) {
+    return ((value / 5) * 100).toString() + '%';
+  }
+
   render() {
     var quality = this.props.productQualityMetaData;
     var size = this.props.productSizeMetaData;
@@ -14,42 +18,113 @@ export default class ProductBreakdown extends React.Component {
     var fit = this.props.productFitMetaData;
     var length = this.props.productLengthMetaData;
     console.log('quality: ', quality);
+    console.log('size: ', size);
+    console.log('comfort: ', comfort);
+    console.log('width: ', width);
+    console.log('fit: ', fit);
+    console.log('length: ', length);
 
     if (quality) {
       var qualityBreakdown =
-      <div className='qualityBreakdown'>
-        <div className='feedback-average'>{quality.value}</div>
-      </div>
+        <div className='qualityBreakdown'>
+          <div className='characteristic-title'>Quality</div>
+          <div className="side left">
+            <div>Poor</div>
+          </div>
+          <div className='middle'>
+            <div className='characteristic-container'>
+              <div className='quality-pointer' style={{width: this.calculatePointerPosition(quality.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Perfect</div>
+          </div>
+        </div>
     }
     if (size) {
       var sizeBreakdown =
-      <div className='sizeBreakdown'>
-        <div className='feedback-average'>{size.value}</div>
-      </div>
+        <div className='sizeBreakdown'>
+          <div className='characteristic-title'>Size</div>
+          <div className="side left">
+            <div>Too small</div>
+          </div>
+          <div className='middle'>
+          <div className='characteristic-container'>
+              <div className='size-pointer' style={{width: this.calculatePointerPosition(size.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Too big</div>
+          </div>
+        </div>
     }
     if (width) {
       var widthBreakdown =
-      <div className='widthBreakdown'>
-        <div className='feedback-average'>{width.value}</div>
-      </div>
+        <div className='widthBreakdown'>
+          <div className='characteristic-title'>Width</div>
+          <div className="side left">
+            <div>Too narrow</div>
+          </div>
+          <div className='middle'>
+          <div className='characteristic-container'>
+              <div className='width-pointer' style={{width: this.calculatePointerPosition(width.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Too wide</div>
+          </div>
+        </div>
     }
     if (comfort) {
       var comfortBreakdown =
-      <div className='comfortBreakdown'>
-        <div className='feedback-average'>{comfort.value}</div>
-      </div>
+        <div className='comfortBreakdown'>
+          <div className='characteristic-title'>Comfort</div>
+          <div className="side left">
+            <div>Uncomfortable</div>
+          </div>
+          <div className='middle'>
+          <div className='characteristic-container'>
+              <div className='comfort-pointer' style={{width: this.calculatePointerPosition(comfort.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Perfect</div>
+          </div>
+        </div>
     }
     if (fit) {
       var fitBreakdown =
-      <div className='fitBreakdown'>
-        <div className='feedback-average'>{fit.value}</div>
-      </div>
+        <div className='fitBreakdown'>
+          <div className='characteristic-title'>Fit</div>
+          <div className="side left">
+            <div>Runs tight</div>
+          </div>
+          <div className='middle'>
+          <div className='characteristic-container'>
+              <div className='fit-pointer' style={{width: this.calculatePointerPosition(fit.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Runs long</div>
+          </div>
+        </div>
     }
     if (length) {
       var lengthBreakdown =
-      <div className='lengthBreakdown'>
-        <div className='feedback-average'>{length.value}</div>
-      </div>
+        <div className='lengthBreakdown'>
+          <div className='characteristic-title'>Length</div>
+          <div className="side left">
+            <div>Runs short</div>
+          </div>
+          <div className='middle'>
+          <div className='characteristic-container'>
+              <div className='length-pointer' style={{width: this.calculatePointerPosition(length.value)}}> &#9660;</div>
+            </div>
+          </div>
+          <div className="side right">
+            <div>Runs long</div>
+          </div>
+        </div>
     }
     return (
       <div id='characteristicsSummary'>
