518607dab1634087ad0e4a84870c9324a79c8c8a
Nick Hays <nicholasahays@gmail.com>
2022-08-26 23:16:45 -0700

    Adds skuId to data attribute on option and passes skuId to handleSizeQuantity function for the cart POST

--- client/src/modules/ProductOverview/Cart/Size.jsx
+++ client/src/modules/ProductOverview/Cart/Size.jsx
@@ -7,11 +7,17 @@ export default function Size(props) {
 
   return (
     <label>Size:
-      <select id='sizeSelector' onChange={(e)=>props.handleSizeQuantity({size: e.target.value, quantity: e.target.selectedOptions[0].attributes[0].value })}>
+      <select id='sizeSelector' onChange={(e)=>props.handleSizeQuantity(
+          {
+            size: e.target.value,
+            quantity: e.target.selectedOptions[0].dataset.quantity,
+            skuId: e.target.selectedOptions[0].dataset.skuid
+          }
+        )}>
 
         <option value='selectsize'>Select Size</option>
         {Object.entries(props.style.skus).filter(sku => sku[1].quantity > 0).map((sku, i) => (  //check quantity > 0
-          <option data-quantity={sku[1].quantity} key={i}>{sku[1].size}</option>
+          <option data-quantity={sku[1].quantity} data-skuid={sku[0]} key={i}>{sku[1].size}</option>
         ))}
 
       </select>
