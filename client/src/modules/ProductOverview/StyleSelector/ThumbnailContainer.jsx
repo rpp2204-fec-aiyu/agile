360385e2a6c579f79198be2f0ef68c787c9830e5
Nick Hays <nicholasahays@gmail.com>
2022-08-31 11:55:16 -0700

    Refactors to use Thumbnail Component instead of returning <img> element

--- client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
+++ client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Thumbnail from './Thumbnail.jsx'
 
 export default function ThumbnailContainer(props) {
 
@@ -17,15 +18,11 @@ export default function ThumbnailContainer(props) {
       <h4><strong>Style > </strong>{title}</h4>
       <div style={{display: 'flex', flexWrap: 'wrap', width: '330px', gap: '10px' }}>
       {props.styles.map(style => (
-        <img src={style.photos[0].thumbnail_url}
-             onClick={()=>handleClick(style)}
-             width='75'
-             height='75'
-             style={{borderRadius: '50%', cursor:'pointer', }}
-             key={style.style_id}
-             className='product-style_thumnbnail'
-             >
-             </img>
+        <Thumbnail style={style}
+                   key={style.style_id}
+                   handleClick={handleClick}
+                   src={style.photos[0].thumbnail_url}
+        />
       ))}
       </div>
     </div>
