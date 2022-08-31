6fe68441fc8a4bb9c70e6e3f4bd17cda4b290c34
Nick Hays <nicholasahays@gmail.com>
2022-08-31 11:59:50 -0700

    Refactors StyleSelector Componenent to be Functional Component

--- client/src/modules/ProductOverview/StyleSelector/StyleSelector.jsx
+++ client/src/modules/ProductOverview/StyleSelector/StyleSelector.jsx
@@ -2,43 +2,12 @@ import React from 'react'
 import axios from 'axios'
 import ThumbnailContainer from './ThumbnailContainer.jsx'
 
-export default class StyleSelector extends React.Component {
-  constructor(props) {
-    super(props)
+export default function StyleSelector(props) {
 
-    // this.state = {
-    //   //photos: [],
-    //   styles: []
+  return (
+    <>
+      <ThumbnailContainer styles={props.styles} setStyle={props.setStyle} setPrice={props.setPrice} setSalePrice={props.setSalePrice}/>
+    </>
+  )
 
-    // }
-
-  }
-
-  // componentDidMount() {
-  //   let id = this.props.id
-  //   console.log('ID FROM PROPS: ', id)
-  //   axios.get(`/products/${id}`)
-  //     .then(results => {
-  //       console.log('STYLE',results.data)
-  //       //let styles = []
-  //       // results.data.results.forEach(style => {
-  //       //   thumbnails.push(style.photos[0].thumbnail_url)
-  //       // })
-  //       this.setState({
-  //         styles: results.data.results
-  //       }, () => console.log(this.state.styles))
-  //     })
-  // }
-
-  render() {
-    return (
-      <div>
-        {/*<Title /> */}
-        {/*this.state.photos.length ? <ThumbnailContainer photos={this.state.photos}/> : null*/}
-        {/*{this.state.styles.length ? <ThumbnailContainer styles={this.state.styles}/> : null} */}
-        <ThumbnailContainer styles={this.props.styles} setStyle={this.props.setStyle} setPrice={this.props.setPrice} setSalePrice={this.props.setSalePrice}/>
-
-      </div>
-    )
-  }
 }
