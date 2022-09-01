4d6d4e49606fc745923ff42ae12a399ab6c79954
Nick Hays <nicholasahays@gmail.com>
2022-09-01 13:10:18 -0700

    Makes top margin 30px;

--- client/src/modules/ProductOverview/ProductInfo/Overview.jsx
+++ client/src/modules/ProductOverview/ProductInfo/Overview.jsx
@@ -4,17 +4,16 @@ export default function Overview(props) {
 
 
   return (
-    <div style={{display: 'flex', position: 'relative'}}>
-      <div style={{marginLeft: '120px'}} >
+    <div style={{display: 'flex', position: 'relative', marginTop: '30px', color: '#25383C'}}>
+      <div style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'column', marginLeft: '120px', width: '550px'}} >
         <h3>{props.slogan}</h3>
 
         <p>
           {props.description}
         </p>
       </div>
-      <div style={{borderLeft: '2px solid #696969', marginRight: '150px'}}>
+      <div style={{borderLeft: '2px solid #696969', position: 'absolute', left: '700px'}}>
         <ul id='featuresList'>
-
           {props.features.map((feature, i) => {
             return (<li key={i}>{feature.feature}: {feature.value}</li>)
           })}
