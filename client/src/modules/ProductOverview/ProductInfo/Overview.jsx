ecc14b60dda005da01852db5923791733ce75890
Nick Hays <nicholasahays@gmail.com>
2022-09-01 11:43:22 -0700

    Adds left border to features list

--- client/src/modules/ProductOverview/ProductInfo/Overview.jsx
+++ client/src/modules/ProductOverview/ProductInfo/Overview.jsx
@@ -4,19 +4,23 @@ export default function Overview(props) {
 
 
   return (
-    <div>
-      <h3>{props.slogan}</h3>
+    <div style={{display: 'flex', position: 'relative'}}>
+      <div >
+        <h3>{props.slogan}</h3>
 
-      <p>
-        {props.description}
-      </p>
-      <ul>
-        <label>--Features--
-        {props.features.map((feature, i) => {
-          return (<li key={i}>{feature.feature}: {feature.value}</li>)
-        })}
-        </label>
-      </ul>
+        <p>
+          {props.description}
+        </p>
+      </div>
+      <div style={{borderLeft: '1px solid #000000'}}>
+        <ul id='featuresList'>
+
+          {props.features.map((feature, i) => {
+            return (<li key={i}>{feature.feature}: {feature.value}</li>)
+          })}
+
+        </ul>
+      </div>
     </div>
   )
 }
