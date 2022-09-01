1e61bb9437c8d438f369ec560c9f1a30efadc47a
Nick Hays <nicholasahays@gmail.com>
2022-09-01 14:48:38 -0700

    Adds useEffects to update height of gallery image

--- client/src/modules/ProductOverview/Gallery/Gallery.jsx
+++ client/src/modules/ProductOverview/Gallery/Gallery.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 export default function Gallery(props) {
 
   const [galleryPhoto, setGalleryPhoto] = useState(props.style.photos[0].url)
+  const [imageHeight, setImageHeight] = useState(null)
 
   function handleClick(photo) {
     setGalleryPhoto(photo)
@@ -12,11 +13,28 @@ export default function Gallery(props) {
     setGalleryPhoto(props.style.photos[0].url)
   },[props.style])
 
+  let img;
+  let height;
+
+  useEffect(() => {
+    img = document.getElementById('galleryMainImage')
+    height = img.clientHeight
+    setImageHeight(height)
+    console.log('HEIGHT',height)
+  }, [])
+
+  useEffect(() => {
+    img = document.getElementById('galleryMainImage')
+    height = img.clientHeight
+    setImageHeight(height)
+    console.log('HEIGHT',height)
+  }, [galleryPhoto])
+
+
   return (
-    <div className='gallery'>
-      <img className='galleryMainImage' src={galleryPhoto} //style={{maxWidth: '50%'}}
-          key={props.style.style_id}>
-      </img>
+    <div id='gallery'>
+      <img id='galleryMainImage' src={galleryPhoto} />
+      {/* key={props.style.style_id}> */}
       <br></br>
       <div className='galleryThumbnailContainer'>
         {props.style.photos.map((photo, i) => (
