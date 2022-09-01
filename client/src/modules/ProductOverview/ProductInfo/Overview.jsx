eebdc314c16f57b1bb524dd4a760d7004267fbbf
Nick Hays <nicholasahays@gmail.com>
2022-09-01 11:49:27 -0700

    Adds margins to both sides of product info

--- client/src/modules/ProductOverview/ProductInfo/Overview.jsx
+++ client/src/modules/ProductOverview/ProductInfo/Overview.jsx
@@ -5,14 +5,14 @@ export default function Overview(props) {
 
   return (
     <div style={{display: 'flex', position: 'relative'}}>
-      <div >
+      <div style={{marginLeft: '120px'}} >
         <h3>{props.slogan}</h3>
 
         <p>
           {props.description}
         </p>
       </div>
-      <div style={{borderLeft: '1px solid #000000'}}>
+      <div style={{borderLeft: '1px solid #000000', marginRight: '150px'}}>
         <ul id='featuresList'>
 
           {props.features.map((feature, i) => {
