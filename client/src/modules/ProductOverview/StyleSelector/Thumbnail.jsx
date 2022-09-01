27188c22d7a9b5130361a763ecc0f9ced9fbbc22
Nick Hays <nicholasahays@gmail.com>
2022-08-31 17:33:50 -0700

    Adds objectFit attribute to crop image, and Removes erroneous key prop

--- client/src/modules/ProductOverview/StyleSelector/Thumbnail.jsx
+++ client/src/modules/ProductOverview/StyleSelector/Thumbnail.jsx
@@ -7,8 +7,7 @@ export default function Thumbnail(props) {
         onClick={()=>props.handleClick(props.style)}
         width='75'
         height='75'
-        style={{borderRadius: '50%', cursor:'pointer', }}
-        key={props.key}
+        style={{borderRadius: '50%', cursor:'pointer', objectFit: 'cover' }}
         className='product-style_thumnbnail'
     />
   )
