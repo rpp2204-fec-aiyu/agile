347a05e90a8be87877c17bbf7ab2f775477babbf
Nick Hays <nicholasahays@gmail.com>
2022-09-08 14:49:41 -0700

    Renders checkmark on click, but currently in wrong position

--- client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
+++ client/src/modules/ProductOverview/StyleSelector/ThumbnailContainer.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import Thumbnail from './Thumbnail.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function ThumbnailContainer(props) {
 
   const [title, setTitle] = useState(props.styles[0].name)
+  const [highlightedThumbnail, setHighlightedThumbnail] = useState(0)
 
-  function handleClick(style) {
+
+
+  function handleClick(style, index) {
     setTitle(style.name)
     props.setStyle(style)
 
     props.setPrice(style.original_price)
     props.setSalePrice(style.sale_price)
+    setHighlightedThumbnail(highlightedThumbnail === index ? null : index)
   }
 
   return (
@@ -19,12 +24,28 @@ export default function ThumbnailContainer(props) {
       <br/>
       <br/>
       <div id='stylesThumbnailContainer'>
-      {props.styles.map(style => (
-        <Thumbnail style={style}
-                   key={style.style_id}
-                   handleClick={handleClick}
-                   src={style.photos[0].thumbnail_url}
-        />
+      {props.styles.map((style, i)=> (
+        <div onClick={()=>handleClick(style, i)}>
+          {highlightedThumbnail === i ?
+            <div position='relative'>
+              <FontAwesomeIcon icon="fa-solid fa-circle-check" position='absolute' left='20px' top='20px' />
+              <Thumbnail style={style}
+                        index={i}
+                        key={style.style_id}
+                        //handleClick={handleClick}
+                        src={style.photos[0].thumbnail_url}
+              />
+            </div>
+            :
+            <Thumbnail style={style}
+                      index={i}
+                      key={style.style_id}
+                      //handleClick={handleClick}
+                      src={style.photos[0].thumbnail_url}
+            />
+          }
+
+        </div>
       ))}
       </div>
     </div>
