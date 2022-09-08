ed647cc817cf0d7514f8d4310972623d5485f733
Nick Hays <nicholasahays@gmail.com>
2022-09-08 00:28:45 -0700

    Creates font awesome library file

---
+++ client/src/modules/ProductOverview/fontawesome.js
@@ -0,0 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faAngleUp,
+  faAngleDown,
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faAngleUp,
+  faAngleDown,
+  faChevronUp,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight
+)
