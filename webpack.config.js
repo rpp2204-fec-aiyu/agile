c4394a7fb64347ed014ce95aa5083e69db0f5c83
Nick Hays <nicholasahays@gmail.com>
2022-09-15 14:52:10 -0700

    Removes webpack compression plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,12 @@
 const path = require('path')
-const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './client/src/index.jsx',
   output: {
     path: path.resolve(__dirname + '/client/dist'),
     filename: 'bundle.js'
-
   },
-  plugins: [
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /.js$|.css$/,
-    })
-  ],
   module: {
     rules: [
       {
