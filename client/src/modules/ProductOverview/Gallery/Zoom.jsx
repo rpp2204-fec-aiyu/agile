a5fb97e3965779f6727788eb1be4734cb35d3a8b
Nick Hays <nicholasahays@gmail.com>
2022-09-16 11:30:37 -0700

    Working Zoom view

--- client/src/modules/ProductOverview/Gallery/Zoom.jsx
+++ client/src/modules/ProductOverview/Gallery/Zoom.jsx
@@ -4,17 +4,18 @@ import React, { useState, useEffect } from 'react'
 export default function Zoom(props) {
 
   //let background = "url(" + props.galleryPhoto + ") no-repeat 0 0 fixed"
-  let width;
-  let height;
-  if(props.naturalHeight > props.naturalWidth) {
-     width = '800px'
-     height = '1000px'
-  } else {
-    width = '1000px'
-    height = '800px'
-  }
+  let width = '1500px';
+  let height = '800px';
+  // if(props.naturalHeight > props.naturalWidth) {
+  //    width = '800px'
+  //    height = '1000px'
+  // } else {
+  //   width = '1000px'
+  //   height = '800px'
+  // }
   return (
     <div id='zoomContainer'
+         onClick={props.expandView}
          style={{
            border: '3px solid black',
            //objectFit: 'cover',
@@ -23,16 +24,18 @@ export default function Zoom(props) {
            //backgroundSize: 'cover',
            backgroundSize: `${props.naturalWidth * 2.5}px ${props.naturalHeight * 2.5}px`,
            backgroundImage: `url(${props.galleryPhoto})`,
-           //backgroundColor: 'red',
            height: 'auto',
-           left: '0',
+           //left: '0',
            minHeight: height,
            minWidth: width,
+          //  marginLeft: 'auto',
+          //  marginRight: 'auto',
            overflow: 'hidden',
-           //position: 'fixed',
-           top: '1000',
-           //width: '100%',
-           //zIndex: '10'
+           backgroundPosition: 'center',
+           position: 'absolute',
+           //top: '15px',
+           width: '100%',
+           zIndex: '10'
          }}
          >
       {/* <img  src={props.galleryPhoto} width='1750' height='1500' style={{objectFit: 'cover'}} /> */}
