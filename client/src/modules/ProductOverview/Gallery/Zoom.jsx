307ac100b378ae6e4f0a6158e7f6c896aae6940d
Nick Hays <nicholasahays@gmail.com>
2022-09-15 21:10:56 -0700

    Makes backgroundSize 2.5x the natwidth and natheight

--- client/src/modules/ProductOverview/Gallery/Zoom.jsx
+++ client/src/modules/ProductOverview/Gallery/Zoom.jsx
@@ -4,6 +4,15 @@ import React, { useState, useEffect } from 'react'
 export default function Zoom(props) {
 
   //let background = "url(" + props.galleryPhoto + ") no-repeat 0 0 fixed"
+  let width;
+  let height;
+  if(props.naturalHeight > props.naturalWidth) {
+     width = '800px'
+     height = '1000px'
+  } else {
+    width = '1000px'
+    height = '800px'
+  }
   return (
     <div id='zoomContainer'
          style={{
@@ -11,12 +20,14 @@ export default function Zoom(props) {
            //objectFit: 'cover',
            backgroundRepeat: 'no-repeat',
            //backgroundAttachment: 'fixed',
+           //backgroundSize: 'cover',
+           backgroundSize: `${props.naturalWidth * 2.5}px ${props.naturalHeight * 2.5}px`,
            backgroundImage: `url(${props.galleryPhoto})`,
            //backgroundColor: 'red',
            height: 'auto',
            left: '0',
-           minHeight: '800px',
-           minWidth: '900px',
+           minHeight: height,
+           minWidth: width,
            overflow: 'hidden',
            //position: 'fixed',
            top: '1000',
